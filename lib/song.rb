321c152655c4c038efd3c69993585bd745779b05
Dang Nguyen <danghwin@gmail.com>
2020-05-29 22:19:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,6 +12,14 @@ class Song
   def self.all
     @@all 
   end 
+  
+   def self.new_by_filename(file)
+    artist_name = file.split(" - ")[0]
+    song_name = file.split(" - ")[1]
+    song = Song.new(song_name)
+    song.artist_name = artist_name
+    song
+  end
 
   
   
