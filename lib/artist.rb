a4656a8829da8c336a720acf3b57d1c47d4b53e2
Dang Nguyen <danghwin@gmail.com>
2020-05-29 22:06:38 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -18,12 +18,18 @@ class Artist
   @songs << song 
  end 
  
-  def self.find(name)
-    self.all.detect { |artist| artist.name == name }
-  end 
-  
-   def self.find_or_create_by_name(name)
-     self.find(name) || self.create(name)
+      def self.find(name)
+        self.all.detect { |artist| artist.name == name }
+      end 
+      
+      def self.create(name)
+        artist = self.new(name)
+        artist.name = name 
+        artist 
+      end 
+    
+       def self.find_or_create_by_name(name)
+         self.find(name) || self.create(name)
    end
    
    
